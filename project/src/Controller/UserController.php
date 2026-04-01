218d197fd5e33fc21fce22a986c18ccd4ab4a9bd
JeremyByrdyCheron <jbc60510@outlook.fr>
2026-04-01 15:54:49 +0200

    User can select forgotten password that send him an email to reset his password

---
+++ project/src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class UserController extends AbstractController
+{
+    #[IsGranted('ROLE_CONNECTED_USER')]
+    #[Route('/user', name: 'user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
