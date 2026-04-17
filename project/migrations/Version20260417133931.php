105bb3d8dd9f1111b044a9c96122434055fac413
JeremyByrdyCheron <jbc60510@outlook.fr>
2026-04-17 15:41:14 +0200

    V1

--- project/migrations/Version20260417133931.php
+++ project/migrations/Version20260417133931.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260416073520 extends AbstractMigration
+final class Version20260417133931 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,23 @@ final class Version20260416073520 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE announcement (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_date DATETIME NOT NULL, category VARCHAR(255) NOT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, updated_at DATETIME DEFAULT NULL, author_id INT DEFAULT NULL, interested_user_id INT DEFAULT NULL, INDEX IDX_4DB9D91CF675F31B (author_id), INDEX IDX_4DB9D91CB54B2A53 (interested_user_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE announcement (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, created_date DATETIME NOT NULL, category VARCHAR(255) NOT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, updated_at DATETIME DEFAULT NULL, author_id INT NOT NULL, INDEX IDX_4DB9D91CF675F31B (author_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE announcement_user (interested_user_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_A1A2DE15B54B2A53 (interested_user_id), INDEX IDX_A1A2DE15A76ED395 (user_id), PRIMARY KEY (interested_user_id, user_id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, inscription_date DATE NOT NULL, is_verified TINYINT NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE announcement ADD CONSTRAINT FK_4DB9D91CB54B2A53 FOREIGN KEY (interested_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE announcement_user ADD CONSTRAINT FK_A1A2DE15B54B2A53 FOREIGN KEY (interested_user_id) REFERENCES announcement (id)');
+        $this->addSql('ALTER TABLE announcement_user ADD CONSTRAINT FK_A1A2DE15A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE announcement DROP FOREIGN KEY FK_4DB9D91CF675F31B');
-        $this->addSql('ALTER TABLE announcement DROP FOREIGN KEY FK_4DB9D91CB54B2A53');
+        $this->addSql('ALTER TABLE announcement_user DROP FOREIGN KEY FK_A1A2DE15B54B2A53');
+        $this->addSql('ALTER TABLE announcement_user DROP FOREIGN KEY FK_A1A2DE15A76ED395');
         $this->addSql('DROP TABLE announcement');
+        $this->addSql('DROP TABLE announcement_user');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE messenger_messages');
     }
